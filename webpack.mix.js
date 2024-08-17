ae66fb09c1e05826ad0ee2f25a02cdf8e3a6b0e2
Rumesh Amarasinghe <rumesh.a@eyepax.com>
2024-08-17 18:43:50 +0530

    changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,28 +1,29 @@
-let tailwindcss = require('tailwindcss');
-let mix = require('laravel-mix');
-require('laravel-mix-tailwind');
+let tailwindcss = require("tailwindcss");
+let mix = require("laravel-mix");
+require("laravel-mix-tailwind");
 
-mix.js('./client/src/js/app.js', './dist/js/app.min.js')
+mix
+  .js("./client/js/app.js", "./dist/js/app.min.js")
 
-	.copy('./client/src/images', './dist/images')
-	.copy('./client/src/fonts', './dist/fonts')
-	.sass('./client/src/scss/base.scss', './dist/css/base.css')
-	.sass('./client/src/scss/global.scss', './dist/css/global.css')
-	.sass('./client/src/scss/app.scss', './dist/css/app.css')
-	.tailwind()
+  .copy("./client/images", "./dist/images")
+  .copy("./client/fonts", "./dist/fonts")
+  .sass("./client/scss/base.scss", "./dist/css/base.css")
+  .sass("./client/scss/global.scss", "./dist/css/global.css")
+  .sass("./client/scss/app.scss", "./dist/css/app.css")
+  .tailwind()
 
-	// Options
-	.sourceMaps()
-	.options({
-		processCssUrls: false,
-		postCss: [
-			require('autoprefixer'), // auto prefix
-			require('postcss-pxtorem')({
-				// convert all pixel values to rem
-				rootValue: 16,
-				propList: ['*'],
-			}),
-		],
-		clearConsole: true,
-		publicPath: '',
-	});
+  // Options
+  .sourceMaps()
+  .options({
+    processCssUrls: false,
+    postCss: [
+      require("autoprefixer"), // auto prefix
+      require("postcss-pxtorem")({
+        // convert all pixel values to rem
+        rootValue: 16,
+        propList: ["*"],
+      }),
+    ],
+    clearConsole: true,
+    publicPath: "",
+  });
